2170a792eee8c6ed0f05a835d078222bd351b911
buihien0109 <49893899+buihien0109@users.noreply.github.com>
2022-10-20 11:27:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,12 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults({
-    noCors : true
-})
+const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+server.use(jsonServer.bodyParser)
 server.use(router)
+
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
