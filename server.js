3ef4e32e6a725b01dda8d520f9a907fa101beda8
buihien0109 <49893899+buihien0109@users.noreply.github.com>
2022-10-20 11:15:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
-const jsonServer = require("json-server");
-const databaseFile = "database.json";
-const server = jsonServer.create();
-const router = jsonServer.router(databaseFile);
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const adapter = new FileSync(databaseFile);
-const db = low(adapter);
-
-// Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares);
-
-// Handle POST, PUT and PATCH request
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.use(jsonServer.bodyParser);
-
-// Setup others routes
-server.use(router);
-
-// Start server
-server.listen(port, () => {
-    console.log("Server is running on port " + port);
-});
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
 module.exports = server
